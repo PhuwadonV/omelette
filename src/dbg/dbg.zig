54b5c9ae078a689a3048fd92698bef63169bd8a1
Phuwadon Verasertniyom <phuwadonpeak@hotmail.com>
2026-04-09 12:37:34 +0700

    Refactor: update show error code format

--- src/dbg/dbg.zig
+++ src/dbg/dbg.zig
@@ -8,18 +8,38 @@ const unicode = std.unicode;
 const utf8ToUtf16LeStringLiteral = unicode.utf8ToUtf16LeStringLiteral;
 
 pub fn showMsg(msg: [:0]const u8) void {
-    _ = wnd.MessageBoxA(null, msg, app.spec.getAppTitle(), wnd.MB_OK);
+    _ = wnd.MessageBoxA(
+        null,
+        msg,
+        app.spec.getAppTitle(),
+        wnd.MB_OK,
+    );
 }
 
 pub fn showMsgW(msg: [:0]const u16) void {
-    _ = wnd.MessageBoxW(null, msg, utf8ToUtf16LeStringLiteral(app.spec.getAppTitle()), wnd.MB_OK);
+    _ = wnd.MessageBoxW(
+        null,
+        msg,
+        utf8ToUtf16LeStringLiteral(app.spec.getAppTitle()),
+        wnd.MB_OK,
+    );
 }
 
 pub fn showErrorMsg(msg: [:0]const u8) void {
     if (config.dev_mode) {
-        if (wnd.MessageBoxA(null, msg, "Skip this error breakpoint?", wnd.MB_YESNO | wnd.MB_ICONERROR) == wnd.IDNO) @breakpoint();
+        if (wnd.MessageBoxA(
+            null,
+            msg,
+            "Skip this error breakpoint?",
+            wnd.MB_YESNO | wnd.MB_ICONERROR,
+        ) == wnd.IDNO) @breakpoint();
     } else {
-        _ = wnd.MessageBoxA(null, msg, app.spec.getAppTitle(), wnd.MB_ICONERROR);
+        _ = wnd.MessageBoxA(
+            null,
+            msg,
+            app.spec.getAppTitle(),
+            wnd.MB_ICONERROR,
+        );
     }
 }
 
